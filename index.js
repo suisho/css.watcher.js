abc009db689bfb903a3f8eed146078ccb6ef4668
bnwemon <bnwemon@gmail.com>
2012-06-01 00:07:00 +0900

    add socket

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 var server = require("./server");
+var socket = require("./server/socket.js");
 var conf = {
   "directory" : "./test/less/"
 };
 server.setConfig(conf);
 server.startServer(8910);
+socket.init(8911);
