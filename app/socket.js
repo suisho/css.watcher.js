740e298f54c22e382c1c495feafc2056f6237f06
bnwemon <bnwemon@gmail.com>
2012-06-02 02:49:25 +0900

    change less change emit func

--- app/socket.js
+++ app/socket.js
@@ -1,21 +1,18 @@
 //socket
 var socket_io = require('socket.io');
-var io = null;
 
-exports.lessChange = function(){
-  console.error("this function is not prepare");
+var io = null;
+exports.lessChange = function(file){
+  console.log(io.sockets);
+  io.sockets.emit('less_refresh', { file : file });
 };
 
 exports.listen = function(server){
   console.log("listen socket");
   io = socket_io.listen(server);
   io.set("origin","*");
+  io.set("log level",2);
   io.sockets.on('connection', function (socket) {
-    console.log("aaaa");
-    socket.broadcast.emit('init', { hello: 'world' });
-    //add lessChange function
-    exports.lessChange = function(file){
-      socket.emit('less_refresh', { file : file });
-    }
+    //socket.emit('init', { hello: 'world' });
   });
 }
